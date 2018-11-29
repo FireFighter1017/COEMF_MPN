851be132638a203eda02e5699cc16b4ac0ee7895
Pascal Bellerose <pascal_bellerose@cascades.com>
2018-11-29 14:13:24 -0500

    Updated model and model update routines

--- scripts/mm_addVendorToModel.R
+++ scripts/mm_addVendorToModel.R
@@ -1,28 +1,76 @@
-mm_addVendorToModel <- function(name, no, freq){
-  ## retreive vendor association from model
+mm_addVendorToModel <- function(name, Z011No, freq, alias){
+  # Updates the model based on mapping received
+  #
+  # Args:
+  #   name:   Z011 Vendor name to map
+  #   Z011No:     Z011 Vendor number to map
+  #   freq:   Number of observations in current sample
+  #   alias:  Vendor name used as alias
+  #
+  # Returns:
+  #   nothing
+  
+  
+  # Bootstrapping
   source("https://raw.githubusercontent.com/FireFighter1017/COEMF/master/funcs/lubripack.r")
   source("https://raw.githubusercontent.com/FireFighter1017/COEMF/master/MasterData/funcs/normVendor.r")
   lubripack("readr", "dplyr", "tidyr", "stringdist")
   
-  ## Read mappings
+  # Read model
   mapProb <- read.csv("./cache/SAPMpnMappingsFrequency.csv",
-                      encoding="UTF-8")
-  ## filter on vendor mappings
-  vendorMaps <- mapProb$Z011VendorNo==no
-  ## adjust N, prob for existing mappings
-  mapProb$N[vendorMaps] <- mapProb$N[vendorMaps] + freq
-  mapProb$prob[vendorMaps] <- mapProb$n[vendorMaps] / mapProb$N[vendorMaps]
+                      encoding="UTF-8",
+                      stringsAsFactors=FALSE)
   
-  ## Set n, weight, prob and weightedCsore for new mapping
-  newMap <- mapProb[vendorMaps,][1,]
-  newMap$VendorNames <- normVendor(name)
-  newMap$n <- freq
-  dist <- unlist(stringdist(name, newMap$Z011VendorName))
-  newMap$weight <- if_else(dist==0, 1, 1/dist)
-  newMap$prob <- newMap$n/newMap$N
-  newMap$weightedScore <- newMap$weight * newMap$prob
-  mapProb <- rbind(mapProb, newMap)
+  # Update mappings statistics 
+  # (N, prob, weightedScore)
   
+  ## filter on vendor mappings other than the one received
+  vendorAliases <- mapProb$Z011VendorNo==Z011No & !(normVendor(mapProb$VendorNames)==normVendor(alias))
+  
+  if(nrow(mapProb[vendorAliases,])>0){
+    ## adjust N, prob for existing mappings
+    N <- mapProb$N[vendorAliases][1] + freq
+    mapProb$N[vendorAliases] <- 
+    mapProb$prob[vendorAliases] <- mapProb$n[vendorAliases] / mapProb$N[vendorAliases]
+    mapProb$weightedScore[vendorAliases] <- mapProb$prob[vendorAliases] * mapProb$weight[vendorAliases]
+  }
+
+  mapExists <- nrow(mapProb[((mapProb$Z011VendorNo == Z011No) & (mapProb$Z011VendorName == name) & (mapProb$VendorNames == alias)
+                           ),]
+                    ) > 0
+  
+  if(mapExists){
+    
+    ## If mappings exists, we must update its statistics
+    updMap <- mapProb[mapProb$Z011VendorNo        == Z011No & 
+                      mapProb$Z011VendorName      == name &
+                      mapProb$VendorNames         == alias,
+                      ]
+    updMap$n <- updMap$n + freq
+    updMap$prob <- updMap$n/updMap$N
+    updMap$N <- updMap$N + freq
+    updMap$weightedScore <- updMap$weight * updMap$prob
+    mapProb[mapProb$Z011VendorNo==Z011No & 
+            mapProb$Z011VendorName==name &
+            mapProb$VendorNames==alias,] <- updMap
+    
+  }else{
+  
+    ## NEW MAPPING
+    ## Set n, weight, prob and weightedScsore for new mapping
+    newMap <- mapProb[1,]
+    newMap$Z011VendorNo <- Z011No
+    newMap$Z011VendorName <- name
+    newMap$VendorNames <- alias
+    newMap$n <- freq
+    dist <- unlist(adist(name, newMap$Z011VendorName))
+    newMap$weight <- if_else(dist==0, 1, 1/dist)
+    newMap$N <- freq
+    newMap$prob <- newMap$n/newMap$N
+    newMap$weightedScore <- newMap$weight * newMap$prob
+    mapProb <- rbind(mapProb, newMap)
+    
+  }
   write_csv(mapProb, "./cache/SAPMpnMappingsFrequency.csv")
   
 }
--- scripts/mm_processNewMaterials.r
+++ scripts/mm_processNewMaterials.r
@@ -1,3 +1,10 @@
+## tODO
+## recalculer n, N, weight, prob et weighted Score quand on traite un mapping
+## Tester et confirmer le fonctionnement de l'Algorithme 
+##   de mise à jour du modèle de prédiction
+## Créer une application Shiny ou autre qui pourra être hébergée 
+##   dans le cloud ou sur un serveur Cascades et fonctionnelle sur demande
+
 source("https://raw.githubusercontent.com/FireFighter1017/COEMF/master/funcs/lubripack.r")
 source("https://raw.githubusercontent.com/FireFighter1017/COEMF/master/MasterData/funcs/normVendor.r")
 source("https://raw.githubusercontent.com/FireFighter1017/COEMF_MPN/master/scripts/mm_matchToZ011.r")
@@ -22,20 +29,23 @@ rm(newFile)
 
 # Process each file
 for(filename in files){
-  # execute function that will process the materials and generate the output
+  # Read input file having load format to SAP
   newFile <- read.csv(paste(newDir, filename, sep=""), encoding="UTF-8")
   colnames(newFile)[1] <- "MATNR"
+  # Select only relevant columns
   newFile <- newFile[,c("MATNR", "MAKTX_EN", "MAKTX_FR", "MFRNR", "MFRPN")]
+  # Find matching names in model and join values
   newFile$normVendors <- normVendor(newFile$MFRNR)
-  mappedVendors <- left_join(newFile, mapProb, by=c("normVendors"="VendorNames"))
+  mappedVendors <<- left_join(newFile, mapProb, by=c("normVendors"="VendorNames"))
   
-  ## Filter those that did not find a match
+  # For those that did not find a matching model
+  # we perform Levenshtein matching with Z011 reference list
   if(sum(is.na(mappedVendors$Z011VendorNo)) > 0){
-    unmapd <- mappedVendors[is.na(mappedVendors$Z011VendorNo),]
-    unmapd <- unmapd %>% group_by(MFRNR) %>% summarize(n=n())
+    unmapd <<- mappedVendors[is.na(mappedVendors$Z011VendorNo),]
+    unmapd <<- unmapd %>% group_by(MFRNR) %>% summarize(n=n())
     ## Try to match them using Levenshtein
-    matchResults <- matchToZ011(unmapd$MFRNR)
-    View(cbind(matchResults, unmapd))
+    matchResults <<- matchToZ011(unmapd$MFRNR)
+    unMapped <- cbind(matchResults, unmapd)
     write_csv(cbind(matchResults, unmapd), 
               paste("./srcData/complete/unmapped_", 
                     filename, 
@@ -52,7 +62,34 @@ for(filename in files){
   
 }
 
-unMapped <- rbind(read_csv("srcData/complete/unmapped_Copy of 08_SPARE_PARTS_1715_V682.csv"),
-                  read_csv("srcData/complete/unmapped_Copy of 08_SPARE_PARTS_2771_V651.csv")
-)
-dist <- stringdist(unMapped$MFRNR, mapProb, method='jw')
+readMapped <- function(srcDir){
+  files <- list.files(srcDir, pattern="^mapped_")
+  # Process each file
+  for(filename in files){
+    mapped <- read.csv(paste(srcDir, filename, sep=""), 
+                       encoding="UTF-8",
+                       stringsAsFactors=FALSE)
+    mapped <- mapped[!is.na(mapped$Z011VendorNo),]
+    ## Update model with recent mappings
+    for(i in c(1:nrow(mapped))){
+      map <- mapped[i,]
+      n <- nrow(mapped[mapped$MFRNR==map$MFRNR,])
+      mm_addVendorToModel(map$Z011VendorName, 
+                          map$Z011VendorNo,
+                          n,
+                          map$MFRNR
+                          )
+    }
+  }
+}
+
+## Once unMapped values are mapped and approved, we can update the model
+#mm_addVendorToModel(unMapped$NAME[2,1], 
+#                    unMapped$VENDOR[2,2], 
+#                    unMapped$n[2,3], 
+#                    unMapped$MFRNR[3])
+
+# unMapped <- rbind(read_csv("srcData/complete/unmapped_Copy of 08_SPARE_PARTS_1715_V682.csv"),
+#                   read_csv("srcData/complete/unmapped_Copy of 08_SPARE_PARTS_2771_V651.csv")
+# )
+# dist <- stringdist(unMapped$MFRNR, mapProb, method='jw')
